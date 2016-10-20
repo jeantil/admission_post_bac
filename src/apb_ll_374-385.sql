133114ca5c580638e969536ba05d5791f4905483
Julien Férard <julien_ferard@yahoo.fr>
2016-10-21 00:04:38 +0200

    X = 13

---
+++ src/apb_ll_374-385.sql
@@ -0,0 +1,19 @@
+/* Code fourni, lignes 374 à 385 */
+X:='13';
+retour:=pk_new_classement_commun.valid_classement_formation(
+  l_g_ea_cod_ges, o_g_ea_cod_ins, o_g_ti_cod, 5,
+  login, type_login, mode_dev,
+  confirm, saio, niр,
+  0, indic,
+  mess_err, mess_aff);
+
+IF retour!=0
+  THEN ROLLBACK;
+  RETURN retour;
+END IF;
+
+/*
+Explication détaillée :
+Comme au bloc précédent, la valeur retour prend la valeur de retour d'une fonction, valid_classement_formation(...). Le nom fait moins penser à un effet de bord, mais c'est à nouveau une possiblité.
+A nouveau, si l'opération se passe mal (retour différent de 0), tout est annulé.
+*/
